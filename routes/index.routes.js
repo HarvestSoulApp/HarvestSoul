623ce26b9a5fe7689e23f9beef0b7e459f9af53d
David Mullally <mullallydavid7@gmail.com>
2023-01-11 12:57:31 +0100

    fix bug with edit profile/events

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const isLoggedOut = require("../middleware/isLoggedOut");
+const isLoggedIn = require("../middleware/isLoggedIn");
+const isProfileOwner = require("../middleware/isProfileOwner");
+
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  let loggedOut;
+  if (!req.session) {
+    loggedOut = true;
+  }
+  else {
+    loggedOut = false
+  }
+  res.render("index" , loggedOut);
 });
 
 module.exports = router;
