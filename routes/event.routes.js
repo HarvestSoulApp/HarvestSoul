290188c3644b06280c1120ba4dc4ee15e06ffb24
David Mullally <mullallydavid7@gmail.com>
2023-01-12 17:14:44 +0100

    fix delete route

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -91,24 +91,39 @@ const currentUserId = currentUser._id;
     Event.findById(eventId)
         .then((event) => {
             //we need to take the interested array and get all names of interested users by their Id
+            console.log(event)
             const comparisonResult = currentUserId === event.organizer._id.toString()
-            console.log(event.organizer._id.toString())
+            // console.log(event.organizer._id.toString())
             const {date, description, location, _id, interested, organizerName, imageUrl } = event
             // const date = event.date.toISOString().split('T')[0] //this formats the date object into string yyyy-mm-dd
             User.find({_id:{$in:interested}}).then((users) => {
                 const usernames = users.map(user => user.username)
-                res.render('event/event', {date, description, location, _id, currentUserId, usernames, organizerName, imageUrl, comparisonResult})
+                res.render('event/event', {
+                    date, description, location, _id, currentUserId, usernames, organizerName, imageUrl,
+                    comparisonResult
+                })
             })
         })
     
 })
 
-router.post('/:eventId/delete', isLoggedIn, isEventOrganizer, (req, res, next) => {
+router.post('/:eventId/delete', isLoggedIn, (req, res, next) => {
     const eventId = req.params.eventId
-    Event.findByIdAndDelete(eventId)
-        .then(() => { res.redirect('/event/eventCreate') })
-    
+    Event.findOneAndDelete({ _id: eventId, organizer: req.session.currentUser._id })
+        .then((event) => {
+            if (!event) {
+                res.redirect('/auth/unauthorized')
+            }
+            res.redirect('/event/find')
+        })
+        .catch(error => {
+            console.log('error deleting the event', error)
+            next(error)
+         })
+        
 });
+    
+
 
 //adds loged in user id to the interested property of respective event if unique
 router.post('/:eventId/subscribeUser/:userId', isLoggedIn, (req, res, next) => {
